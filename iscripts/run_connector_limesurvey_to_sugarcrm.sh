6a01ebb8e8c0571102dcc9c7f93f09132ac29240
marciojv <marcio@ambientelivre.com.br>
2013-07-26 12:56:23 -0300

    adicionado scripts de jobs

---
+++ iscripts/run_connector_limesurvey_to_sugarcrm.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+# Iguana 
+#
+# Iguana is MPL 2.0  http://www.mozilla.org/MPL/2.0/
+#  marcio@ambientelivre.com.br 
+#
+
+cd /opt/iguana/iscripts
+
+DIR_IGUANA=/opt/iguana
+
+$DIR_IGUANA/idevelopers/etl/lib/data-integration/pan.sh -file $DIR_IGUANA/idevelopers/etl/modules/connectors-plugins/limesurvey-to-sugarcrmsurveys_to_sugarcrm_dynamic.ktr
+
+
+
+
+
